ca5136eb716956edaaf45e4ae54fd508ee4549b9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-22 14:59:44 +0100

    Run commands async so they don't block the UI

--- src/Shy.hs
+++ src/Shy.hs
@@ -1,15 +1,17 @@
 module Shy (main) where
 
 import qualified Brick
+import qualified Brick.BChan
 import qualified Brick.Widgets.Edit as Edit
+import qualified Control.Concurrent.Async as Async
 import Control.Monad.IO.Class (liftIO)
 import Data.Function ((&))
+import qualified Data.IORef
 import qualified Data.Maybe
 import qualified Data.Text as Text
 import qualified Data.Text.IO
 import qualified Data.Text.Lazy
 import qualified Data.Text.Lazy.Encoding
-import qualified Data.Void as Void
 import qualified Graphics.Vty as Vty
 import qualified Graphics.Vty.Input.Events as VtyEvents
 import qualified System.Exit
@@ -19,16 +21,18 @@ import qualified System.Process.Typed as Process
 main :: IO ()
 main = do
   initialVty <- mkVty
-  endState <- Brick.customMain initialVty mkVty Nothing app initialState
+  chan <- Brick.BChan.newBChan 1
+  endState <- Brick.customMain initialVty mkVty (Just chan) (app chan) initialState
   let finalCmd = Text.concat (Edit.getEditContents (editor endState))
   liftIO $ Data.Text.IO.putStrLn finalCmd
 
 data State = State
   { editor :: Edit.Editor Text.Text Name,
+    cmdProcess :: Maybe (Async.Async (), Data.IORef.IORef Text.Text),
     output :: Text.Text
   }
 
-type Event = Void.Void
+data Event = CmdProcessFinished
 
 data Name = Editor
   deriving (Eq, Ord, Show)
@@ -39,18 +43,28 @@ mkVty = Vty.mkVty Vty.defaultConfig {Vty.outputFd = Just stdError}
 stdError :: System.Posix.Types.Fd
 stdError = System.Posix.Types.Fd 2
 
-app :: Brick.App State Event Name
-app = (Brick.simpleApp Brick.emptyWidget) {Brick.appDraw, Brick.appHandleEvent, Brick.appChooseCursor}
+app :: Brick.BChan.BChan Event -> Brick.App State Event Name
+app chan =
+  (Brick.simpleApp Brick.emptyWidget)
+    { Brick.appDraw,
+      Brick.appHandleEvent = appHandleEvent chan,
+      Brick.appChooseCursor
+    }
 
 initialState :: State
 initialState =
   State
     { editor = Edit.editor Editor (Just 1) "",
+      cmdProcess = Nothing,
       output = ""
     }
 
-appHandleEvent :: State -> Brick.BrickEvent Name Event -> Brick.EventM Name (Brick.Next State)
-appHandleEvent state event =
+appHandleEvent ::
+  Brick.BChan.BChan Event ->
+  State ->
+  Brick.BrickEvent Name Event ->
+  Brick.EventM Name (Brick.Next State)
+appHandleEvent chan state event =
   case event of
     Brick.VtyEvent vtyEvent ->
       case vtyEvent of
@@ -59,9 +73,25 @@ appHandleEvent state event =
         VtyEvents.EvKey VtyEvents.KEnter [] -> Brick.halt state
         _ -> do
           newEditor <- Edit.handleEditorEvent vtyEvent (editor state)
-          newOutput <- liftIO $ runCommand (Text.concat (Edit.getEditContents newEditor))
-          Brick.continue state {editor = newEditor, output = newOutput}
-    Brick.AppEvent appEvent -> Void.absurd appEvent
+          newRef <- liftIO (Data.IORef.newIORef "")
+          newCmdProcess <-
+            liftIO . Async.async $ do
+              case cmdProcess state of
+                Nothing -> pure ()
+                Just (async, _) -> Async.cancel async
+              runCommand chan newRef (Text.concat (Edit.getEditContents newEditor))
+          Brick.continue
+            state
+              { editor = newEditor,
+                cmdProcess = Just (newCmdProcess, newRef),
+                output = ""
+              }
+    Brick.AppEvent CmdProcessFinished -> do
+      newOutput <-
+        case cmdProcess state of
+          Nothing -> pure ""
+          Just (_, ref) -> liftIO (Data.IORef.readIORef ref)
+      Brick.continue state {output = newOutput}
     Brick.MouseDown _ _ _ _ -> Brick.continue state
     Brick.MouseUp _ _ _ -> Brick.continue state
 
@@ -82,8 +112,8 @@ appDraw state =
 appChooseCursor :: State -> [Brick.CursorLocation Name] -> Maybe (Brick.CursorLocation Name)
 appChooseCursor _ = Data.Maybe.listToMaybe
 
-runCommand :: Text.Text -> IO Text.Text
-runCommand cmd = do
+runCommand :: Brick.BChan.BChan Event -> Data.IORef.IORef Text.Text -> Text.Text -> IO ()
+runCommand chan ref cmd = do
   (exitCode, output) <-
     Process.proc "bash" []
       & Process.setStdin (Process.byteStringInput (Data.Text.Lazy.Encoding.encodeUtf8 (Data.Text.Lazy.fromStrict cmd)))
@@ -92,6 +122,10 @@ runCommand cmd = do
         case Data.Text.Lazy.Encoding.decodeUtf8' output of
           Left _ -> "[non-utf8 content]"
           Right txt -> Data.Text.Lazy.toStrict txt
-  case exitCode of
-    System.Exit.ExitSuccess -> pure textOutput
-    System.Exit.ExitFailure code -> pure (textOutput <> "\n[failed with code: " <> Text.pack (show code) <> "]")
+  let fullOutput =
+        case exitCode of
+          System.Exit.ExitSuccess -> textOutput
+          System.Exit.ExitFailure code -> (textOutput <> "\n[failed with code: " <> Text.pack (show code) <> "]")
+  Data.IORef.writeIORef ref fullOutput
+  _ <- Brick.BChan.writeBChanNonBlocking chan CmdProcessFinished
+  pure ()
